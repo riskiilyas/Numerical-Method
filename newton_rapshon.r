e39b063138f3dac7057e3baff0df063383dfcdc4
riskiilyas <riskiilyas03@gmail.com>
2022-09-17 20:55:34 +0700

    Newton Rapson Finish

--- newton_rapshon.r
+++ newton_rapshon.r
@@ -1,38 +1,34 @@
 library(datasets)  # Load/unload base packages manually
 
-
-# wrapper around sprintf() and print()
-printf <- function(...) invisible(cat(sprintf(...)))
-print <- function(...) invisible(cat((...), "\n"))
-
-# f(x) = e^(-x) - x
-f <- function(x) {
-  return(exp(1)^(-x) - x)
-}
-
-# f: function
-# x_min1, xi: initial x value (x-1, x0)
-# n: no. of iterations to perform
-secant <- function(f, xi_min1, xi, n) {
-  trials <- c()
-  x <- c()
+newtonRapson <- function(x, n, f, df) {
+  xList <- c()
   fx <- c()
-  for (i in 1:n) {
-    printf("%03d | x = %.8f | f(x) = %.8f\n", i, xi, f(xi))
-    trials[i] = i
-    x[i] = xi
-    fx[i] =  f(xi)
-    x_plus1 <- xi - ((f(xi) * (xi_min1 - xi)) / (f(xi_min1) - f(xi)))
-    xi_min1 <- xi
-    xi <- x_plus1
+  for(i in 1:n) {
+    xList[i] <- format(round(x, 3), nsmall = 3)
+    fx[i] <- format(round(f(x), 3), nsmall = 3)
+    x <- x - (f(x) / df(x))
   }
   
-  #datas = matrix(c(xCol,fxCol), ncol = 2, nrow = n)
-  #colnames(datas) = c('x', 'F(x)')
-  #rownames(datas) = c(trials)
+  iteration = c(1:n)
+  plot(iteration, fx)
   
-  plot(fx, x)
+  data = matrix(c(xList,fx), n, 2)
+  colnames(data) <- c('x', 'f(x)')
+  rownames(data) <- c(1:n)
+  
+  table = as.table(data)
+  
+  table
 }
 
-secant(f, 0, 1, 10)
+f <- function(x) {
+  return(exp(1)^(-x))
+}
+
+df <- function(x) {
+  return(-exp(1)^(-x))
+}
+
+newtonRapson(0, 10, f, df)
+
 
