143b60757431731f80f1a36a2a4175e8973539ff
riskiilyas <riskiilyas03@gmail.com>
2022-09-17 20:10:42 +0700

    Bolzano Finish

--- bolzano.r
+++ bolzano.r
@@ -6,31 +6,37 @@ bolzano <- function(a, b, n, f) {
     return()
   }
   
-  xList <- c()
-  x <- -1
+  x3List <- c()
+  x3 <- -1
+  x1 <- c()
+  x2 <- c()
   fx <- c()
   
   for (i in 1:n) {
-    x <- (a+b)/2.0
-    fx[i] <- format(round(f(x), 3), nsmall = 3)
-    xList[i] <- format(round(x, 3), nsmall = 3)
+    x3 <- (a+b)/2.0
+    a_ = format(round(a, 3), nsmall = 3)
+    b_ = format(round(b, 3), nsmall = 3)
+    x1[i] <- ifelse(a<b, a_,b_)
+    x2[i] <- ifelse(a<b, b_,a_)
+    fx[i] <- format(round(f(x3), 3), nsmall = 3)
+    x3List[i] <- format(round(x3, 3), nsmall = 3)
     
-    if(f(x) == 0) {
+    if(f(x3) == 0) {
       break
     }
     
-    if(f(a) * f(x) < 0) {
-      b <- x
-    } else if(f(b) * f(x) < 0) {
-      a <- x
+    if(f(a) * f(x3) < 0) {
+      b <- x3
+    } else if(f(b) * f(x3) < 0) {
+      a <- x3
     }
   }
   
   iteration = c(1:n)
   plot(iteration, fx)
   
-  data = matrix(c(xList,fx), n, 2)
-  colnames(data) <- c('x', 'f(x)')
+  data = matrix(c(x1,x2,x3List,fx), n, 4)
+  colnames(data) <- c('x1', 'x2','x3', 'f(x)')
   rownames(data) <- c(1:n)
   
   table = as.table(data)
